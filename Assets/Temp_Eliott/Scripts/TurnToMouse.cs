055bdfc8391cccf67450d705701b834497a43930
Ketebesseh <EliottL.git@gmail.com>
2025-08-10 22:37:20 +0200

    Tests et quelques shaders

--- Assets/Temp_Eliott/Scripts/TurnToMouse.cs
+++ Assets/Temp_Eliott/Scripts/TurnToMouse.cs
@@ -6,12 +6,6 @@ public class TurnToMouse : MonoBehaviour
     public bool lockYOnly = true;
     public bool smooth = true;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-    }
-
-    // Update is called once per frame
     void Update()
     {
         Vector3 targetWorld = MousePositionManager.Instance.MouseWorldPos;
